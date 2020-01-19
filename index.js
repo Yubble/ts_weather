8d8b695e5f238eb53dcc3dda857ab9085aa32925
刘燕保 <liuyanbao@shuidihuzhu.com>
2020-01-19 20:43:10 +0800

    feat: 测试二哈

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+/*
+ * @Author: your name
+ * @Date: 2020-01-19 19:56:12
+ * @LastEditTime : 2020-01-19 20:05:17
+ * @LastEditors  : Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /tsWeatherForecast/ts_weather/index.ts
+ */
+console.log('hello ts-weather');
