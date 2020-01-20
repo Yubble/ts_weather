10039be0478995b223e5c8db2981563c16a24d74
刘燕保 <liuyanbao@shuidihuzhu.com>
2020-01-20 21:13:45 +0800

    feat: 准备新功能

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,14 @@
  * @FilePath: /tsWeatherForecast/ts_weather/index.ts
  */
 
+import axios from "axios";
 import colors from "colors";
 import commander from "commander";
 
 commander
   .version("0.1.0")
-  // .option("-d, --debug", "output extra debugging")
   .option("-c, --city [name]", "Add city name")
+  // 获取命令行参数用方括号和尖括号都行
   // .option("-p, --pizza-type <type>", "flavour of pizza")
   .parse(process.argv);
 
@@ -23,7 +24,44 @@ if (process.argv.slice(2).length === 0) {
   process.exit();
 }
 
+interface IWeatherResponse {
+  status: string;
+  count: string;
+  info: string;
+  infocode: string;
+  lives: ILive[];
+}
+
+interface ILive {
+  province: string;
+  city: string;
+  adcode: string;
+  weather: string;
+  temperature: string;
+  winddirection: string;
+  windpower: string;
+  humidity: string;
+  reporttime: string;
+}
+
+const URL = "http://restapi.amap.com/v3/weather/weatherInfo";
+const KEY = "71bb9f4d12bf747736cf2400ef0334eb";
+
+const log = console.log;
+async function getWeather(city: string) {
+  try {
+    const url = `${URL}?city=${encodeURI(city)}&key=${KEY}`;
+    const response = await axios.get(url);
+    const live = response.data.lives[0];
+    log(colors.yellow(live.reporttime));
+    log(colors.white(`${live.province} ${live.city}`));
+    log(colors.green(`${live.weather} ${live.temperature} 度`));
+  } catch {
+    log(colors.red("天气服务出现异常"));
+  }
+}
+getWeather(commander.city);
 // console.log('opts is ', commander.opts())
-console.log("city is ", commander.city);
+// console.log("city is ", commander.city);
 // console.log('pizza is ', commander.pizzaType)
 // console.log("hello ts-weather");
